1044f432bc07cb344f3346db4d726c48badca533
muztafa74 <you@example.commuztafalap@gmail.com>
2024-09-13 00:38:22 +0300

    first 1.1

--- app.py
+++ app.py
@@ -227,6 +227,11 @@ def translate_text():
         return jsonify({'error': 'Internal server error'}), 500
 
 
+@app.route('/')
+def home():
+    return render_template('index.html')
+    
+
 
 if __name__ == '__main__':
     app.run(debug=True)
